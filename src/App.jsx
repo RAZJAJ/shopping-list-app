5e07e7654c395f0bf446a308a98b8a7a6dffd85d
RAZJAJ <jajiabdulrazaq77@gmail.com>
2025-10-15 14:41:45 +0100

    Added the code for my App.jsx to navigate to my MaainPage

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import { useNavigate } from "react-router-dom";
+
+function App() {
+  const navigate = useNavigate();
+
+  const handleGetStarted = () => {
+    navigate("/main");
+  };
+  
+  return (
+    <div className="min-h-screen bg-[#D9D9D9] flex items-center justify-center py-16 px-6">
+      
+      <div className="bg-[#D9D9D9] rounded-3xl shadow-lg w-full max-w-md flex flex-col justify-between p-10 space-y-16">
+        
+        
+        <div className="space-y-10">
+          
+          <div className="flex justify-center">
+            <h1 className="text-4xl font-bold text-black tracking-wide whitespace-nowrap">
+              SHOPPING LIST
+            </h1>
+          </div>
+
+          
+          <div className="text-4xl space-y-6 px-3">
+            <h2 className="text-lg font-semibold text-black/80 uppercase leading-relaxed">
+              Classify your <br /> shopping easily
+            </h2>
+            <p className="text-sm text-black/70 leading-relaxed">
+              Add, edit, and check your <br />
+              shopping items from <br />
+              anywhere. 
+            </p>
+          </div>
+
+          
+          <div className="flex justify-center pt-6">
+            <button
+        onClick={handleGetStarted}
+        className="bg-black text-white px-6 py-3 rounded-lg hover:bg-gray-800 transition"
+      >
+        Get Started
+      </button>
+          </div>
+        </div>
+
+        
+        <div className="flex justify-evenly items-center gap-16 mt-12">
+          
+          <div className="flex flex-col items-center space-y-4">
+            <div className="bg-white rounded-full p-6 sm:p-8 shadow-lg">
+              <span className="text-3xl sm:text-4xl">➕</span>
+            </div>
+            <span className="text-sm sm:text-base font-medium text-black/80">
+              Add Item
+            </span>
+          </div>
+
+          
+          <div className="flex flex-col items-center space-y-4">
+            <div className="bg-white rounded-full p-6 sm:p-8 shadow-lg">
+              <span className="text-3xl sm:text-4xl">🗑️</span>
+            </div>
+            <span className="text-sm sm:text-base font-medium text-black/80">
+              Delete Item
+            </span>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
